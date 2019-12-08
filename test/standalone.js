c8a8bac428ab1f3a04e97726d5223b3ea973c5e4
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-12-08 17:34:14 +0100

    calculate trx cost

--- test/standalone.js
+++ test/standalone.js
@@ -77,18 +77,18 @@ describe("Remittance", function() {
     it("carol can withdraw if she has the hash", async function() { 
         let carolInitialBalance = await getBalance(carol);        
         await instance.createAccount(passwHash, 1, { from: alice, value:amountToSend }); 
-        let txWithDraw = await instance.withdraw(passw1, passw2, { from: carol});   
-        let carolBalance = await getBalance(carol);        
+        let txWithDrawReceipt = await instance.withdraw(passw1, passw2, { from: carol});  
+        let trx = await getTransaction(txWithDrawReceipt.tx);
+        let carolBalance = await getBalance(carol); 
 
-        let gasUsed = txWithDraw.receipt.gasUsed;
-        let gasPrice = (await getTransaction(txWithDraw.tx)).gasPrice;
-        equalsInWei(expectedBalanceDifference(carolInitialBalance, carolBalance, gasUsed, new BN(gasPrice)), '0.2');
+        let gasUsed = txWithDrawReceipt.receipt.gasUsed;
+        let gasPrice = trx.gasPrice;
 
-
-        truffleAssert.eventEmitted(txWithDraw, 'withdrawEvent', (event) => {
+        truffleAssert.eventEmitted(txWithDrawReceipt, 'withdrawEvent', (event) => {
             return event.passwordHash == passwHash && event.sender == carol && event.amount.toString(10) == amountToSend.toString(10);
         });
-
+        
+        equalsInWei(expectedBalanceDifference(carolInitialBalance, carolBalance, gasUsed, new BN(gasPrice)), '0.2');
         assert.strictEqual((await instance.accounts(passwHash)).amount.toString(10), '0');
         assert.strictEqual((await getBalance(instance.address)).toString(10), '0');
     });
