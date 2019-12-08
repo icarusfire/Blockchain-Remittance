d49af2a60260335ccd67715eb7f8ccd6ae2510f5
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-12-08 16:45:01 +0100

    -add toString(10) in equalsInWei funcxtion

--- test/standalone.js
+++ test/standalone.js
@@ -11,8 +11,11 @@ const assert = require('assert-plus');
 
 Promise = require("bluebird");
 const truffleAssert = require('truffle-assertions');
+const getTransaction =  Promise.promisify(web3.eth.getTransaction);
+
 const getBalance = web3.eth.getBalance;
-const { BN, toWei,fromWei, sha3, fromAscii, soliditySha3 } = web3.utils;
+const toWei = function(val) { return web3.utils.toWei(val, "ether") };
+const { BN,fromWei, sha3, fromAscii, soliditySha3 } = web3.utils;
 const amountToSend = toWei("0.2", "ether");
 
 const passw1String = "abcd";
@@ -20,6 +23,15 @@ const passw2String = "efgh";
 
 const passw1 = web3.utils.asciiToHex(passw1String).padEnd(66, "0");
 const passw2 = web3.utils.asciiToHex(passw2String).padEnd(66, "0");
+
+const equalsInWei = function(val1, val2) { return assert.strictEqual(val1.toString(10), toWei(val2).toString(10)) };
+
+const expectedBalanceDifference = function (initialBalance, balance, gasUsed, gasPrice) {
+     return new BN(balance)
+        .add(new BN(gasUsed)
+        .mul(gasPrice))
+        .sub(new BN(initialBalance)); 
+    }
 
 describe("Remittance", function() {    
     console.log("Current host:", web3.currentProvider.host);
@@ -62,9 +74,16 @@ describe("Remittance", function() {
         assert.strictEqual((await instance.accounts(passwHash)).amount.toString(10), amountToSend.toString(10));
     });
 
-    it("carol can withdraw if she has the hash", async function() {        
+    it("carol can withdraw if she has the hash", async function() { 
+        let carolInitialBalance = await getBalance(carol);        
         await instance.createAccount(passwHash, 1, { from: alice, value:amountToSend }); 
         let txWithDraw = await instance.withdraw(passw1, passw2, { from: carol});   
+        let carolBalance = await getBalance(carol);        
+
+        let gasUsed = txWithDraw.receipt.gasUsed;
+        let gasPrice = (await getTransaction(txWithDraw.tx)).gasPrice;
+        equalsInWei(expectedBalanceDifference(carolInitialBalance, carolBalance, gasUsed, new BN(gasPrice)), '0.2');
+
 
         truffleAssert.eventEmitted(txWithDraw, 'withdrawEvent', (event) => {
             return event.passwordHash == passwHash && event.sender == carol && event.amount.toString(10) == amountToSend.toString(10);
