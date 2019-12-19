e5da06693cb40be6b1e328179c956f8601c003b5
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-12-19 11:53:28 +0100

    add shouldExpire to mockRemittance

--- contracts/RemittanceMock.sol
+++ contracts/RemittanceMock.sol
@@ -4,10 +4,12 @@ import "./Remittance.sol";
 
 contract RemittanceMock is Remittance {
 
+    bool public shouldExpire = true;
+
     constructor(bool _pausable) Remittance(_pausable) public {}
 
     function isExpired(uint expiryTime) public view returns (bool) {
-        return true;
+        return shouldExpire;
     }
 
 }
