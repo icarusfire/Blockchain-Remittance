ec4573a3724ccc2e913af6da2bca2626e1f3db68
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-12-19 15:04:34 +0100

    remove redundant requires

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -74,8 +74,6 @@ contract Remittance is Pausable {
 
     function validateCandidateHash(bytes32 candidateHash, address addr, bytes32 passw) public view returns (bool){
         require(candidateHash > 0, "passwordHash should not be empty");
-        require(passw > 0, "password should not be empty");
-        require(addr != address(0), "address should not be empty");
         require(candidateHash == hashPasswords(addr, passw), "Hashes do not match");
         return true;
     }
